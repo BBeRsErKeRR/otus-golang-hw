ec888026fd9e1b1fd010fc7e2ad5792a634cf990
BBeRsErKeRR <ddarkwatchh@mail.ru>
2022-12-18 23:27:24 +1000

    feat(hw03): add top10 generato

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,41 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
+import (
+	"fmt"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+func Top10(input string) []string {
+	if input != "" {
+		// Generate base structs
+		words := make(map[string]int)
+		re := regexp.MustCompile(`\s+(-\s+)*`)
+		replace := regexp.MustCompile(`[,.!:"']+`)
+		// Collect all words
+		for _, el := range re.Split(replace.ReplaceAllString(input, ""), -1) {
+			if el != "" {
+				words[strings.ToLower(el)]++
+			}
+		}
+		// Check if words more then 10
+		if len(words) >= 10 {
+			// Generate slice of words
+			buffer := make([]string, 0, len(words))
+			for k := range words {
+				buffer = append(buffer, k)
+			}
+			// Sorting
+			sort.Slice(buffer, func(i, j int) bool {
+				return words[buffer[i]] > words[buffer[j]] ||
+					(words[buffer[i]] == words[buffer[j]] && buffer[i] < buffer[j])
+			})
+			// return only first 10 words
+			res := buffer[0:10]
+			fmt.Println(res)
+			return res
+		}
+	}
 	return nil
 }
