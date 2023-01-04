393d1277f606f64fe4192fe2d49f3773f19e3c16
BBeRsErKeRR <ddarkwatchh@mail.ru>
2023-01-04 22:03:37 +1000

    feat(hw04): add DoubleLinkedList implementation

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,98 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	first *ListItem
+	last  *ListItem
+	len   int
+}
+
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	return l.first
+}
+
+func (l *list) Back() *ListItem {
+	return l.last
+}
+
+func (l *list) insertAfter(node *ListItem, newNode *ListItem) {
+	newNode.Prev = node
+	if node.Next == nil {
+		newNode.Next = nil
+		l.last = newNode
+	} else {
+		newNode.Next = node.Next
+		node.Next.Prev = newNode
+	}
+	node.Next = newNode
+}
+
+func (l *list) insertBefore(node *ListItem, newNode *ListItem) {
+	newNode.Next = node
+	if node.Prev == nil {
+		newNode.Prev = nil
+		l.first = newNode
+	} else {
+		newNode.Prev = node.Prev
+		node.Prev.Next = newNode
+	}
+	node.Prev = newNode
+}
+
+func (l *list) insertFront(node *ListItem) {
+	if l.Front() == nil {
+		l.first = node
+		l.last = node
+		node.Prev = nil
+		node.Next = nil
+	} else {
+		l.insertBefore(l.first, node)
+	}
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	var node ListItem
+	node.Value = v
+	l.insertFront(&node)
+	l.len++
+	return &node
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	var node ListItem
+	node.Value = v
+
+	if l.last == nil {
+		l.insertFront(&node)
+	} else {
+		l.insertAfter(l.last, &node)
+	}
+
+	l.len++
+	return &node
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i.Prev == nil {
+		l.first = i.Next
+	} else {
+		i.Prev.Next = i.Next
+	}
+	if i.Next == nil {
+		l.last = i.Prev
+	} else {
+		i.Next.Prev = i.Prev
+	}
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	l.Remove(i)
+	l.insertFront(i)
+	l.len++
 }
 
 func NewList() List {
