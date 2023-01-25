48b81ad2a1cf95822b42d5484da472b147a07d2a
BBeRsErKeRR <ddarkwatchh@mail.ru>
2023-01-26 00:58:09 +1000

    feat(hw05): add base logic

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,8 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +12,69 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	var errorsC int32
+	var lenT int32
+	if int(errorsC) > m {
+		return ErrErrorsLimitExceeded
+	}
+
+	wg := sync.WaitGroup{}
+	ch := make(chan Task, n)
+	wg.Add(n + 1)
+
+	// Producer
+	go func(sendTasksCountAddr *int32, errorsCountAddr *int32) {
+		defer wg.Done()
+		tasksLen := len(tasks)
+		for {
+			// close chan if found errors
+			testE := int(atomic.LoadInt32(errorsCountAddr)) >= m
+			if testE {
+				close(ch)
+				break
+			}
+
+			// Add new items into chan
+			sendTasksCount := int(atomic.LoadInt32(sendTasksCountAddr))
+			test := sendTasksCount < tasksLen
+			if test {
+				ch <- tasks[sendTasksCount]
+				atomic.AddInt32(sendTasksCountAddr, 1)
+			} else {
+				close(ch)
+				break
+			}
+		}
+	}(&lenT, &errorsC)
+
+	// Consumer's
+	for i := 0; i < n; i++ {
+		go func(errorsCountAddr *int32) {
+			defer wg.Done()
+			for {
+				test := int(atomic.LoadInt32(errorsCountAddr)) <= m
+				if test {
+					f, ok := <-ch
+					if !ok {
+						break
+					}
+					err := f()
+					if err != nil {
+						atomic.AddInt32(errorsCountAddr, 1)
+					}
+				} else {
+					break
+				}
+			}
+		}(&errorsC)
+	}
+
+	wg.Wait()
+
+	// Check last errors
+	if int(errorsC) > m {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
