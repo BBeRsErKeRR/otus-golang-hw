8e0d340cae7d1eb7d1b5267f82d44bfde76eef62
BBeRsErKeRR <ddarkwatchh@mail.ru>
2023-02-10 19:17:24 +1000

    feat(hw06): add base logic to resolve hw

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,31 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	// Resolve stage worker
+	worker := func(stage Stage, input In, done In) Out {
+		out := make(Bi)
+		go func() {
+			defer close(out)
+			for r := range stage(input) {
+				select {
+				case <-done:
+					return
+				default:
+					out <- r
+				}
+			}
+		}()
+		return out
+	}
+	// Buffer to reduce stages results into one value
+	// TODO: create reducer function
+	buffer := make(map[int](Out))
+	for i, stage := range stages {
+		if i == 0 {
+			buffer[i] = worker(stage, in, done)
+		} else {
+			buffer[i] = worker(stage, buffer[i-1], done)
+		}
+	}
+	return buffer[len(stages)-1]
 }
